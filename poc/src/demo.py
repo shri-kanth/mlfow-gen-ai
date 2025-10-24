c55492186634e5ea0fbb4f94363d118302be4aa5
srikanth <srikanth.k@zemosolabs.com>
2025-10-24 18:19:05 +0530

    Functional BuiltIn Scorer

--- poc/src/demo.py
+++ poc/src/demo.py
@@ -1,8 +1,7 @@
-from openai import OpenAI
 from dotenv import load_dotenv
-from mlflow.genai.scorers import Correctness, Guidelines
+from mlflow.deployments import get_deploy_client
+from mlflow.genai.scorers import Guidelines
 from mlflow.genai.judges import is_correct
-from mlflow.genai.scorers import scorer
 from typing import Dict, Any
 import pandas as pd
 import mlflow
@@ -13,6 +12,9 @@ import os
 load_dotenv()
 
 # setup
+gateway_uri = os.getenv("MLFLOW_GATEWAY_URI")
+gateway = get_deploy_client(gateway_uri)
+
 tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(tracking_uri)
 mlflow.set_registry_uri(tracking_uri)
@@ -32,26 +34,20 @@ def register_or_update_prompt(prompt_id, prompt_template, commit_message):
     )
     return updated_prompt
 
-# The @mlflow.trace decorator automatically captures the function's inputs,
-# outputs, and any internal LLM calls.
 @mlflow.trace
-def generate_from_prompt(prompt_name, prompt_version, **params):
-    prompt = mlflow.genai.load_prompt(f"prompts:/{prompt_name}/{prompt_version}")
-    client = OpenAI()
-    response = client.chat.completions.create(
-        model="gpt-4",
-        messages=[
-            {"role": "user", "content": prompt.format(**params)}
-        ]
-    )
-    return response.choices[0].message.content
+def generate_from_prompt(prompt):
+    data = dict(messages=[{"role": "user", "content": prompt}])
+    return gateway.predict(endpoint="chat", inputs=data)
 
 def run_experiment_without_dataset(experiment_id, prompt_id, prompt_version, run_id, **params):
     with mlflow.start_run(run_name=run_id, experiment_id=experiment_id) as run:
         mlflow.log_params(params)
-        result = generate_from_prompt(prompt_id, prompt_version, **params)
-        mlflow.log_text(result, "insult_output.txt")
-        return result
+        prompt_template = mlflow.genai.load_prompt(f"prompts:/{prompt_id}/{prompt_version}")
+        prompt = prompt_template.format(**params)
+        result = generate_from_prompt(prompt)
+        mlflow.log_text(str(result), "result.txt")
+        response = result['choices'][0]['message']['content']
+        return response
 
 def create_evaluation_dataset(experiment_id, dataset_name, df):
     dataset = mlflow.genai.datasets.create_dataset(
@@ -63,25 +59,35 @@ def create_evaluation_dataset(experiment_id, dataset_name, df):
     return dataset
 
 def pred_fn(adjective, noun):
-    return generate_from_prompt("insult-generator-prompt", "1", adjective=adjective, noun=noun)
+    params = {
+        "adjective": adjective,
+        "noun": noun        
+    }
+    prompt_template = mlflow.genai.load_prompt(f"prompts:/{prompt_id}/{prompt_version}")
+    prompt = prompt_template.format(**params)
+    return generate_from_prompt(prompt)
 
-@scorer
-def correctness_scorer(inputs: Dict[Any, Any], outputs: Dict[Any, Any], expectations: Dict[Any, Any]):
-    result = is_correct(
-        request=str(inputs),
-        response=outputs,
-        expected_facts=expectations["expected_facts"],
-        model="openai:/gpt-4",
-    ) 
-    return result if result is not None else 'No'
+def get_scorer():
+    judge_model_uri = "openai:/gpt-3.5-turbo"
+    custom_guidelines = [
+        "The response must be in a friendly and informative tone.",
+        "The response must be under 30 words."
+        ]
+    guidelines_scorer = Guidelines(
+        model=judge_model_uri,
+        guidelines=custom_guidelines
+        )
+    return guidelines_scorer
 
 def run_experiment_with_dataset(run_id, experiment_id, prompt_id, prompt_version, dataset_id): 
     with mlflow.start_run(run_name=run_id, experiment_id=experiment_id) as run:
         dataset = mlflow.genai.datasets.get_dataset(dataset_id=dataset_id)
+        # transform the records in the dataset
+        scorer = get_scorer()
         results = mlflow.genai.evaluate(
             data=dataset,
             predict_fn=pred_fn,
-            scorers=[correctness_scorer],
+            scorers=[scorer],
         )
         return results
 
@@ -109,20 +115,19 @@ prompt_version = "1"
 
 # # ==============================================================================
 # # 3. RUN EXPERIMENT WITHOUT DATASET
-# # ==============================================================================
+# # # ==============================================================================
 # params = {
 #     "adjective": "clumsy",
 #     "noun": "pigeon"
 # }
 # result = run_experiment_without_dataset(
 #     experiment_id=experiment_id,
-#     run_id="Insult_Run_Without_Dataset_0",
+#     run_id="Insult_Run_Without_Dataset_12",
 #     prompt_id=prompt_id,
 #     prompt_version=prompt_version,
 #     **params
 # )
 # print(f"Generated Insult without Dataset: {result}")
-
 
 # # ==============================================================================
 # # 4. DATASET
@@ -158,12 +163,12 @@ prompt_version = "1"
 #     df=eval_data)
 # print(f"Created evaluation dataset: {create_dataset_result.dataset_id}")
 # dataset_id = create_dataset_result.dataset_id
-dataset_id = "d-bc7f6787dce14e9ebe845d813bbf10b8"
-# run_exp_res = run_experiment_with_dataset(
-#     run_id="Insult_Run_With_Dataset_4",
-#     experiment_id=experiment_id,
-#     prompt_id=prompt_id,
-#     prompt_version=prompt_version,
-#     dataset_id=dataset_id
-# )
-# # print(run_exp_res)
+dataset_id = "d-1ca7b925e6b047d6bfedeb22bbe8b5a1"
+run_exp_res = run_experiment_with_dataset(
+    run_id="Insult_Run_With_Dataset_1",
+    experiment_id=experiment_id,
+    prompt_id=prompt_id,
+    prompt_version=prompt_version,
+    dataset_id=dataset_id
+)
+# print(run_exp_res)
